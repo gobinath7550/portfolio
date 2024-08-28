5beaa6fc8c8276e3573c93c233c6a9dc731dd513
gobinath7550 <gobinath7550@gmail.com>
2024-08-28 23:01:45 +0530

    Environment changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/portfolio", // Adjust based on your needs
+  assetPrefix: "/portfolio/",
+};
 
 export default nextConfig;
